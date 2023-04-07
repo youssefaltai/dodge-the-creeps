5b80661dfe18a4016b54b6b0624867ade18d30bc
youssef-attai <youssefgalalnazem@gmail.com>
2023-04-07 16:25:12 +0200

    first commit

---
+++ main.gd
@@ -0,0 +1,62 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Music.stream.loop = true
+	$Music.play()
+	$Player.hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func game_over():
+	$Music.stop()
+	$DeathSound.play()
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	
+	
+func new_game():
+	if not $Music.is_playing():
+		$Music.play()	
+	get_tree().call_group("mobs", "queue_free")
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+
+
+func _on_score_timer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_mob_timer_timeout():
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.progress_ratio = randf()
+	
+	var direction = mob_spawn_location.rotation + PI / 2
+	mob.position = mob_spawn_location.position
+	
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+		
+	add_child(mob)	
